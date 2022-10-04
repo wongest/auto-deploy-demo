9b54a7683665a5b27a9979ea00410ce22632d929
Johnny <345375378@qq.com>
2022-10-04 20:26:38 +0800

    新增页面区分分支功能

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    rules: {
+        //在rules中添加自定义规则
+        //关闭组件命名规则
+        "vue/multi-word-component-names": "off",
+    },
+    "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module"
+    },
+    "parser": "vue-eslint-parser"
+}
